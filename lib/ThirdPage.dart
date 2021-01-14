83a093bd1490ba707de54f89bf4d052491719416
rohan <rohan397gupta@gmail.com>
2021-01-14 13:15:42 +0530

    Completed profile section

--- lib/ThirdPage.dart
+++ lib/ThirdPage.dart
@@ -9,6 +9,7 @@ class ThirdRoute extends StatelessWidget {
   Widget build(BuildContext context) {
     bool isButtonPressed = false;
     return Scaffold(
+      resizeToAvoidBottomInset: false,
       drawer: NavDrawer(),
       appBar: AppBar(
         backgroundColor: Color(0xff2657ce),
@@ -22,6 +23,14 @@ class ThirdRoute extends StatelessWidget {
                 child: Column(
               mainAxisAlignment: MainAxisAlignment.start,
               children: <Widget>[
+                SizedBox(
+                  width: 10,
+                  height: 0,
+                  child: Column(
+                    Text('Your Profile'),
+                  ),
+                  // Text('Your Profile'),
+                ),
                 SizedBox(
                   height: 115,
                   width: 115,
@@ -61,48 +70,94 @@ class ThirdRoute extends StatelessWidget {
                 ),
 
                 Container(
-                    child: TextField(
-                  decoration: InputDecoration(
-                    border: OutlineInputBorder(),
-                    labelText: 'Enter Name Here',
-                    hintText: 'Enter Name Here',
-                  ),
-                  autofocus: false,
-                )),
+                  child: SingleChildScrollView(
+                      child: TextField(
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                      labelText: 'Enter Name Here',
+                      hintText: 'Enter Name Here',
+                    ),
+                    autofocus: false,
+                  )),
+                ),
                 // ),
                 SizedBox(
                   width: 20,
                   height: 20,
                 ),
+
                 Container(
-                    child: TextField(
-                  decoration: InputDecoration(
-                    border: OutlineInputBorder(),
-                    labelText: 'Enter Email',
-                    hintText: 'Enter Your Email',
-                  ),
-                  autofocus: false,
-                )),
+                  child: SingleChildScrollView(
+                      child: TextField(
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                      labelText: 'Enter Email',
+                      hintText: 'Enter Your Email',
+                    ),
+                    autofocus: false,
+                  )),
+                ),
                 SizedBox(
                   width: 20,
                   height: 20,
                 ),
                 RaisedButton(
-                    color: Colors.blue,
-                    child: const Text('Submit'),
-                    onPressed: () {
-                      isButtonPressed = !isButtonPressed;
-                    })
+                  onPressed: () {},
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(80.0)),
+                  padding: const EdgeInsets.all(0.0),
+                  child: Ink(
+                    decoration: const BoxDecoration(
+                      gradient: LinearGradient(
+                        colors: <Color>[
+                          // Color(0xFF0D47A1),
+                          Color(0xFF1976D2),
+                          Color(0xFF42A5F5),
+                        ],
+                      ),
+                      borderRadius: BorderRadius.all(Radius.circular(25.0)),
+                    ),
+                    child: Container(
+                      constraints: const BoxConstraints(
+                        maxHeight: 45.0,
+                        maxWidth: 158.0,
+                        //minWidth: 36.0,
+                        //minHeight: 36.0
+                      ), // min sizes for Material buttons
+                      alignment: Alignment.center,
+                      child: const Text(
+                        'Sign Up',
+                        textAlign: TextAlign.justify,
+                      ),
+                    ),
+                  ),
+                )
               ],
             )),
             Container(
               padding: EdgeInsets.all(10),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: <Widget>[
-                  IconButton(
+              child: SingleChildScrollView(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    IconButton(
+                        icon: Icon(
+                          Icons.subscriptions,
+                          color: Color(0xff2657ce).withOpacity(0.5),
+                          size: 40,
+                        ),
+                        onPressed: () {
+                          Navigator.push(
+                            context,
+                            PageRouteBuilder(
+                              pageBuilder: (_, __, ___) => MainPage(),
+                              transitionDuration: Duration(seconds: 0),
+                            ),
+                          );
+                        }),
+                    IconButton(
                       icon: Icon(
-                        Icons.subscriptions,
+                        Icons.play_circle_filled,
                         color: Color(0xff2657ce).withOpacity(0.5),
                         size: 40,
                       ),
@@ -110,52 +165,38 @@ class ThirdRoute extends StatelessWidget {
                         Navigator.push(
                           context,
                           PageRouteBuilder(
-                            pageBuilder: (_, __, ___) => MainPage(),
+                            pageBuilder: (_, __, ___) => SecondRoute(),
                             transitionDuration: Duration(seconds: 0),
                           ),
                         );
-                      }),
-                  IconButton(
-                    icon: Icon(
-                      Icons.play_circle_filled,
-                      color: Color(0xff2657ce).withOpacity(0.5),
-                      size: 40,
+                      },
                     ),
-                    onPressed: () {
-                      Navigator.push(
-                        context,
-                        PageRouteBuilder(
-                          pageBuilder: (_, __, ___) => SecondRoute(),
-                          transitionDuration: Duration(seconds: 0),
-                        ),
-                      );
-                    },
-                  ),
-                  IconButton(
-                    icon: Icon(
-                      Icons.account_circle,
-                      color: Color(0xff2657ce),
-                      size: 40,
+                    IconButton(
+                      icon: Icon(
+                        Icons.account_circle,
+                        color: Color(0xff2657ce),
+                        size: 40,
+                      ),
+                      onPressed: () {},
                     ),
-                    onPressed: () {},
-                  ),
-                  IconButton(
-                    icon: Icon(
-                      Icons.attach_money,
-                      color: Color(0xff2657ce).withOpacity(0.5),
-                      size: 40,
+                    IconButton(
+                      icon: Icon(
+                        Icons.attach_money,
+                        color: Color(0xff2657ce).withOpacity(0.5),
+                        size: 40,
+                      ),
+                      onPressed: () {
+                        Navigator.push(
+                          context,
+                          PageRouteBuilder(
+                            pageBuilder: (_, __, ___) => FourthRoute(),
+                            transitionDuration: Duration(seconds: 0),
+                          ),
+                        );
+                      },
                     ),
-                    onPressed: () {
-                      Navigator.push(
-                        context,
-                        PageRouteBuilder(
-                          pageBuilder: (_, __, ___) => FourthRoute(),
-                          transitionDuration: Duration(seconds: 0),
-                        ),
-                      );
-                    },
-                  ),
-                ],
+                  ],
+                ),
               ),
             ),
           ],
